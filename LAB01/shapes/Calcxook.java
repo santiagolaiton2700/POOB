1ec21d5b828c8bbb24a50a227751831472f828d4
santiagolaiton2700 <santiago.laiton@mail.escuelaing.edu.co>
2020-01-27 16:36:41 -0500

    operaciones

---
+++ LAB01/shapes/Calcxook.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class xook here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Calcxook
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class xook
+     */
+    public Calcxook()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    public int suma(int numero1,int numero2)
+    {
+        return numero1 + numero2;
+    }
+    public int resta(int numero1,int numero2)
+    {
+        return numero1 - numero2;
+    }
+    public int producto(int numero1,int numero2)
+    {
+        return numero1 * numero2;
+    }
+    public int division(int numero1,int numero2)
+    {
+        return numero1 / numero2;
+    }
+}
